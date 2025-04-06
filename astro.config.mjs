3b86894060ba2d4e7db0eed3a7e0b9214d2db63e
Denis Cazzulo <deniscazzulo@icloud.com>
2025-04-06 02:12:56 +0200

    Rimosso logo e immagini non più utilizzati; aggiornato il favicon e i riferimenti alle immagini nel layout e nei componenti.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://monviso.vercel.app',
+  site: 'https://www.monvisomultiservizi.it',
   vite: {
     plugins: [tailwindcss()]
   },
